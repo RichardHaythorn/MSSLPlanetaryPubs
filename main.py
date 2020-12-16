28630fcabfa66792520914956c14ccf62073693c
Richard Haythornthwaite <45592645+RichardHaythorn@users.noreply.github.com>
2020-12-16 13:29:23 +0000

    Added code for first attempt at output strings
    Signed-off-by: Richard Haythornthwaite <45592645+RichardHaythorn@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,29 +2,41 @@ import requests
 import json
 from datetime import date
 
+f = open("MSSLPlanetaryPubs.txt", "w", encoding="utf-8")
+
 currentyear = date.today().year
+years = range(currentyear, currentyear - 5, -1)
 
 lib_token = '6PTOklOwoNVpIZlkzfX2imWZS1wuJyZ4uCZxijQR'  # Needs Updating for new people
 
 numberofrequestrows = 500
 r = requests.get("https://api.adsabs.harvard.edu/v1/biblib/libraries/MLEF82wzRk60fgarKZPCow",
                  headers={"Authorization": "Bearer " + lib_token},
-                 params={"rows": numberofrequestrows, "fl": "title,year"})
+                 params={"rows": numberofrequestrows, "fl": "author,title,pub_raw,doi,pubdate,property,year"})
 
-# payload = {"fl":"year",
-#            "sort": "first_author asc",
-#            "format": "%i, %T, %Q, doi:%d, %D"}
 paperlist = r.json()['solr']['response']['docs']
 
-for paper in paperlist:
-    if int(paper['year']) < (currentyear - 5):
-        continue
-    else:
-        print(paper['title'][0], paper['year'])
+for year in years:
+    f.write(str(year) + "\n")
+    refereedlist = []
+    nonrefereedlist = []
+    for paper in paperlist:
+        if int(paper['year']) != year:
+            continue
+        else:
+            if "REFEREED" in paper['property']:
+                citationstring = u'{},{},{},doi:{},{}'.format(paper['author'], paper['title'][0], paper['pub_raw'],
+                                                              paper['doi'][0], paper['pubdate'])
+                print(citationstring)
+                f.write(citationstring + "\n")
+    for paper in paperlist:
+        if int(paper['year']) != year:
+            continue
+        else:
+            if "NOT REFEREED" in paper['property']:
+                citationstring = u'{},{},{},{}'.format(paper['author'], paper['title'][0], paper['pub_raw'],
+                                                       paper['pubdate'])
+                print(citationstring)
+                f.write(citationstring + "\n")
 
-# f = open("MSSLPlanetaryPubs.txt", "a")
-# f.write("\n")
-# f.close()
-
-
-# %i, %T, %Q, doi:%d, %D
+f.close()
